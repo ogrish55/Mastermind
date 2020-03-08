211ec833ae2288d7a897955eed74e61027266dff
Kristoffer K <Kristoffer92k@hotmail.com>
2020-03-08 11:35:13 +0100

    Small changes

---
+++ GUI/Help.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GUI
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bodyText = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bodyText
+            // 
+            this.bodyText.AutoSize = true;
+            this.bodyText.Location = new System.Drawing.Point(51, 164);
+            this.bodyText.Name = "bodyText";
+            this.bodyText.Size = new System.Drawing.Size(10, 13);
+            this.bodyText.TabIndex = 0;
+            this.bodyText.Text = "t";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Title.Font = new System.Drawing.Font("MS PGothic", 45F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(67, 44);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(328, 62);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "Mastermind";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(480, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.bodyText);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Help";
+            this.Text = "Help";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bodyText;
+        private System.Windows.Forms.Label Title;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ GUI/Help.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Help : Form
+    {
+        public Help()
+        {
+            InitializeComponent();
+            bodyText.Text = "Mastermind is a code-breaking game.\n" +
+                "The player has 10 attempts to guess the secret combination of colors.\n" +
+                "If a color is guessed, but the position is wrong, a white point will be rewarded.\n" +
+                "If a color and it's position is guessed, a black point will be rewarded.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //new Menu().Show();
+            this.Dispose();
+        }
+    }
+}
